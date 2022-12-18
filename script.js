a8aad5c5e90696bcef9c6d16495f136cd0bdabdd
S-Core21 <ogbonnashedrach8@gmail.com>
2022-12-18 14:09:07 +0100

    modify

--- script.js
+++ script.js
@@ -1,20 +1,74 @@
 let nextReview = document.getElementById('next')
+let prevReview = document.getElementById('prev')
 let reviews = document.querySelectorAll('.card1')
-let reviewMove = false
+let reviewMoveNext = false
+let reviewMovePrev = false
+let reviewMove;
 
 
-for(let i=0; i<reviews.length; i++){
-nextReview.addEventListener('click', ()=>{ 
-    if(reviewMove == false){
-    reviews[i].style.cssText = 'transform: translateX(-120%); transition: 1s linear'
-    reviewMove = true
+
+
+
+function next(){
+    if(reviewMoveNext === false){
+    for(let i=0; i<reviews.length; i++){
+        reviews[i].style.cssText = 'transform: translateX(-110%); transition: 1s linear'
+    }
+    prevReview.style.visibility = 'visible'
+    prevReview.style.transition = '.5s linear'
+    reviewMoveNext = true
+    reviewMove = 1;
 }
-if(reviewMove == true){
-    reviews[i].style.cssText = 'transform: translateX(-220%); transition: 1s linear'
-    reviewMove = false
+
+else if(reviewMoveNext === true){
+    for(let i=0; i<reviews.length; i++){
+        reviews[i].style.cssText = 'transform: translateX(-220%); transition: 1s linear'
+    }
+    reviewMoveNext = true
+    nextReview.style.visibility = 'hidden'
+    
 }
+}
+
+nextReview.addEventListener('click', ()=>{
+        return next()
 })
+
+function prev(){
+    if(reviewMovePrev === false && reviewMove === 1){
+        for(let i=0; i<reviews.length; i++){
+            reviews[i].style.cssText = 'transform: translateX(-110%); transition: 1s linear'
+        }
+        nextReview.style.visibility = 'visible'
+        reviewMovePrev = true
+        reviewMoveNext = false
+        
+    }
+    else if(reviewMovePrev === true){
+        for(let i=0; i<reviews.length; i++){
+            reviews[i].style.cssText = 'transform: translateX(0%); transition: 1s linear'
+
+        }
+        reviewMovePrev = false
+        reviewMoveNext = false
+        prevReview.style.visibility = 'hidden'
+        nextReview.style.visibility = 'visible'
+    }
+}
+ prevReview.addEventListener('click', ()=>{
+    return prev()
+ })
+
+let communityHeader = document.getElementById('dive')
+let teachingHeader = document.getElementById('teaching')
+let phoneSize = window.matchMedia('(max-width:760px)')
+
+if(phoneSize.matches){
+    communityHeader.textContent = 'OUR COMMUNITIES'
 }
 
+if(phoneSize.matches){
+    teachingHeader.textContent = 'WHAT WE TEACH'
+}
 
 console.log(reviews)
