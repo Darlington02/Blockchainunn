406890fbdb95f7624952839c096b7bc731881cfa
S-Core21 <ogbonnashedrach8@gmail.com>
2022-12-22 10:40:37 +0100

    modify

--- team.js
+++ team.js
@@ -1,6 +1,18 @@
 let header = document.querySelector('h1')
 let phoneSize = window.matchMedia('(max-width:760px)')
+let menubar = document.getElementById('menu')
+let closeBar = document.getElementById('close')
+let navList = document.getElementById('navList')
 
 if(phoneSize.matches){
     header.textContent = 'LEADERSHIP AND TEAM'
+}
+
+function menuOpen(){
+    navList.style.cssText = 'transform: translateX(0%)'
+    closeBar.style.cssText = 'transform: translateX(0%)'
+}
+function menuClose(){
+    navList.style.cssText = 'transform: translateX(100%)'
+    closeBar.style.cssText = 'transform: translateX(300%)'
 }
